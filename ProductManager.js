35445c4e10d346feb018ae9adc93f154c9bef695
jmiguelmorralla <jmiguelmorralla@gmail.com>
2024-03-06 20:03:54 -0300

    Rectificación de productos.

--- ProductManager.js
+++ ProductManager.js
@@ -29,32 +29,32 @@ gestorDeProductos.create({
 
 gestorDeProductos.create({
     photo: "foto7.jpg",
-    title: "Rompecabezas",
+    title: "Bloques",
     category: "plastico",
     price: 7000,
     stock: 36
 })
 
 gestorDeProductos.create({
-    photo: "foto55.jpg",
-    title: "Rompecabezas",
-    category: "plastico",
-    price: 7000,
-    stock: 36
+    photo: "foto3.jpg",
+    title: "Casita",
+    category: "madera",
+    price: 6500,
+    stock: 22
 })
 
 gestorDeProductos.create({
-    photo: "foto55.jpg",
-    title: "Rompecabezas",
-    category: "plastico",
-    price: 7000,
-    stock: 36
+    photo: "luna4.jpg",
+    title: "Luna",
+    category: "madera",
+    price: 7500,
+    stock: 39
 })
 
 gestorDeProductos.create({
-    photo: "foto55.jpg",
-    title: "Rompecabezas",
-    category: "plastico",
+    photo: "arcoiris.jpg",
+    title: "Arcoiris",
+    category: "madera",
     price: 7000,
     stock: 36
 })
