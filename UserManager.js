fdba523e6a0c057b49bc38530a0103bf98c0999b
jmiguelmorralla <jmiguelmorralla@gmail.com>
2024-03-06 18:32:30 -0300

    User Manager y users.

---
+++ UserManager.js
@@ -0,0 +1,34 @@
+class UserManager {
+    static #users = []
+    create(data){
+        const user = {
+            id: UserManager.#users.length === 0 ? 1 : UserManager.#users[UserManager.#users.length - 1].id+1,
+            foto: data.foto,
+            email: data.email,
+            password: data.password,
+            role: 0
+        };
+    UserManager.#users.push(user);
+    console.log("Usuario Creado");
+    }
+read(){
+    return UserManager.#users
+}
+}
+
+const gestorDeUsuarios = new UserManager()
+gestorDeUsuarios.create({
+    foto: "foto.jpg",
+    email: "juan@gmail.com",
+    password: "holapepito@",
+})
+
+gestorDeUsuarios.create({
+    foto: "fotoperfil.jpg",
+    email: "cecilia@gmail.com",
+    password: "Cecilia123",
+})
+
+
+
+console.log(gestorDeUsuarios.read())
