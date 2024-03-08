4567965fdd3d93a03fb593370e80b0410b835d02
jmiguelmorralla <114706321+jmiguelmorralla@users.noreply.github.com>
2024-03-08 18:55:30 -0300

    Establecimiento de campos obligatorios tanto en productos como en usuarios. (#3)
    * Corrección de características.
    
    * Establecimiento de campos obligatorios.

--- ProductManager.js
+++ ProductManager.js
@@ -1,62 +1,69 @@
 class ProductManager {
-    static #products = []
-    create(data){
-        const product = {
-            id: ProductManager.#products.length === 0 ? 1 : ProductManager.#products[ProductManager.#products.length - 1].id+1,
-            photo: data.photo,
-            title: data.title,
-            category: data.category,
-            price: data.price,
-            stock: data.stock
-        };
-    ProductManager.#products.push(product);
+  static #products = [];
+  create(data) {
+    const product = {
+      id:
+        ProductManager.#products.length === 0
+          ? 1
+          : ProductManager.#products[ProductManager.#products.length - 1].id +
+            1,
+      title: data.title,
+      photo: data.photo,
+      category: data.category,
+      price: data.price,
+      stock: data.stock,
+    };
+
+    !data.stock || !data.title || !data.photo || !data.category || !data.price
+      ? console.log("Producto no creado. Ingrese todos los datos.")
+      : ProductManager.#products.push(product);
     console.log("Producto Creado");
-    }
-read(){
-    return ProductManager.#products
+  }
+  read() {
+    return ProductManager.#products;
+  }
 }
-}
 
-const gestorDeProductos = new ProductManager()
-
-gestorDeProductos.create({
-    photo: "foto55.jpg",
-    title: "Balancin",
-    category: "madera",
-    price: 15000,
-    stock: 25
-})
+const gestorDeProductos = new ProductManager();
 
 gestorDeProductos.create({
-    photo: "foto7.jpg",
-    title: "Bloques",
-    category: "plastico",
-    price: 7000,
-    stock: 36
-})
+  photo: "foto55.jpg",
+  title: "Balancin",
+  category: "madera",
+  price: 15000,
+  stock: 41,
+});
 
 gestorDeProductos.create({
-    photo: "foto3.jpg",
-    title: "Casita",
-    category: "madera",
-    price: 6500,
-    stock: 22
-})
+  photo: "foto7.jpg",
+  title: "Bloques",
+  category: "plastico",
+  price: 7000,
+  stock: 36,
+});
 
 gestorDeProductos.create({
-    photo: "luna4.jpg",
-    title: "Luna",
-    category: "madera",
-    price: 7500,
-    stock: 39
-})
+  photo: "foto3.jpg",
+  title: "Casita",
+  category: "madera",
+  price: 6500,
+  stock: 22,
+});
 
 gestorDeProductos.create({
-    photo: "arcoiris.jpg",
-    title: "Arcoiris",
-    category: "madera",
-    price: 7000,
-    stock: 36
-})
+  photo: "luna4.jpg",
+  title: "Luna",
+  category: "madera",
+  price: 7500,
+  stock: 39,
+});
 
-console.log(gestorDeProductos.read())
+gestorDeProductos.create({
+  photo: "arcoiris.jpg",
+  title: "Arcoiris",
+  category: "madera",
+  price: 7000,
+  stock: 36,
+});
+
+console.log(gestorDeProductos.read());
