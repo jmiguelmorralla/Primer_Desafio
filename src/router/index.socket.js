0186db96035ae6414c50b6a9461859ae8ba487d2
jmiguelmorralla <jmiguelmorralla@gmail.com>
2024-04-08 18:12:59 -0300

    Formulario en user/register, productos en página de inicio.

--- src/router/index.socket.js
+++ src/router/index.socket.js
@@ -1,6 +1,8 @@
 import usersManager from "../data/fs/UsersManager.fs.js";
+import productsManager from "../data/fs/ProductsManager.fs.js";
 
 export default async (socket) => {
   console.log("client socket " + socket.id);
   socket.emit("users", await usersManager.read());
+  socket.emit("products", await productsManager.read())
 };
