05ccaaf8a9d5e58b443d300db7cf475fe0480e27
jmiguelmorralla <jmiguelmorralla@gmail.com>
2024-04-24 18:39:51 -0300

    Creación de cart.model.js, carts.api.js y cartsmanager.mongo.js

---
+++ src/data/mongo/models/cart.model.js
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+
+const collection = "carts";
+const schema = new Schema(
+  {
+    user_id: { type: String, required: true },
+    product_id: {type: String, required: true,
+    quantity: {type: Number, required: true, default: 1}
+    },
+    state: { type: String, default: "reserved", enum: ["reserved", "paid", "delivered"] },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Cart = model(collection, schema);
+
+export default Cart;
--- src/router/api/products.api.js
+++ src/router/api/products.api.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 // import productsManager from "../../data/fs/ProductsManager.fs.js";
-import productsManager from "../../data/mongo/ProductsManager.mongo.js";
+import productsManager from "../../data/mongo/managers/ProductsManager.mongo.js";
 
 const productsRouter = Router();
 
